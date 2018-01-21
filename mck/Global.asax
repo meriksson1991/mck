2428a48fab748c5fe95bf9f2e7f6f3a67150a7fc
Martin Eriksson (Stratiteq) <meriksson@stratiteq.com>
2018-01-21 16:40:33 +0100

    Add project files.

---
+++ mck/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mck.MvcApplication" Language="C#" %>
---
+++ mck/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mck.Startup))]
+namespace mck
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
